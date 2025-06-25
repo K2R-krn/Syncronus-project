f8db9adfa57be397b3e9b84f05f1cff3ff75d3b2
Karansinh Rathod <115916190+K2R-krn@users.noreply.github.com>
2025-06-25 21:30:24 +0530

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,7 +1,7 @@
 // export const HOST = import.meta.env.VITE_SERVER_URL; // vite url as using vite..
 // export const BASE_URL= import.meta.env.VITE_SERVER_URL;
 
-export const HOST = `https://syncronus-be-production.up.railway.app`;
+export const HOST = `https://syncronus-be-production.up.railway.app/`;
 
 export const AUTH_ROUTES = `${HOST}api/auth`;
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
